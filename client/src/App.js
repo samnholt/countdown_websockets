9a300df8ee9cfc5436e14f45d2dc96dc9d7a2415
Sam Holt <sam@carbon.coop>
2023-08-28 15:25:34 +0100

    Components and get vowels working

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useCallback, useRef} from 'react';
+import { useState } from 'react';
 import useWebSocket from "react-use-websocket";
 
 import logo from './logo.svg';
@@ -16,22 +17,103 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TitleSection />
+        <Game />
     </div>
   );
 }
+
+
+function Game() {
+    return (
+        <>
+            <StartButton />
+            <Vowels />
+            <Play />
+            <CountdownTimer />
+            <Score />
+        </>
+    )
+}
+
+function Play() {
+    return (
+        <>
+        <Letters />
+        <Guess />
+        </>
+    )
+}
+
+function TitleSection () {
+    return <h1>Countdown Game!</h1>
+}
+
+function StartButton() {
+    const { sendJsonMessage } = useWebSocket(WS_URL, {share: true})
+    const handleClickStartGame = useCallback(() => sendJsonMessage(
+        {
+            'type': 'start_game'
+        }), []);
+
+    return <button onClick={handleClickStartGame}>Start Game</button>
+}
+
+function Vowels() {
+
+    const inputRef = useRef(null);
+    const { sendJsonMessage } = useWebSocket(WS_URL, {share: true})
+
+
+    const handleClick = () => {
+        // setUpdated(inputRef.current.value);
+        sendJsonMessage({
+            'type': 'display',
+            'vowels_chosen': parseInt(inputRef.current.value)
+        })
+    }
+
+    return (
+        <>
+            <label>
+                Enter chosen amount of Vowels:
+                <input ref={inputRef}></input>
+                <button id="sendVowels" onClick={handleClick}>Submit Vowels</button>
+            </label>
+        </>
+    )
+}
+
+function Letters() {
+    return (
+        <>
+            <h2>Your letters:</h2>
+            <p id="letterString"></p>
+        </>
+    )
+}
+
+function Guess() {
+    return (
+        <>
+            <label htmlFor="guessedWord">Enter your guess:</label>
+            <input type="text" id="guessedWord"></input>
+            <button id="submitGuessButton">Submit Guess</button>
+        </>
+    )
+}
+
+function CountdownTimer() {
+    return (
+        <>
+        <h3>Time left:</h3>
+        </>
+    )
+}
+
+function Score () {
+    return <h3>Your score:</h3>
+}
+
 
 export default App;
